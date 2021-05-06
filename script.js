0324456e9aaddf63a54a9e3b7f50ffa408fc24bf
Temirlan Amerzhan <>
2021-05-06 13:30:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var age = promptNumber('How old are you?');
+
+if (age >= 17) {
+  alert('You can see an R rated movie alone')
+} else if (age >= 13) {
+  alert('You can see a PG-13 movie alone)
+} else {
+  alert('You can see a G rated movie'
+}
